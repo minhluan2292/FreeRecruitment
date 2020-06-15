b2d63c7712045e910ef854a3f912227305ea5520
minhluan2292 <minhluan2292@gmail.com>
2020-06-15 15:13:03 +0700

    Add Vuetify plugin

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,7 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import Vuetify from '../plugins/vuetify'
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -20,6 +21,9 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('account-management', require('./components/AccountManagement.vue').default);
+Vue.component('vue-container', require('./components/vueContainer.vue').default);
+
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -28,5 +32,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app',
+    vuetify: Vuetify,
+    el: '#app'
 });
---
+++ resources/js/components/AccountManagement.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="account-management">
+
+    </div>
+</template>
+
+<script>
+    
+</script>
+
+<style lang="scss" scoped>
+.chat-app {
+    display: flex;
+}
+</style>
