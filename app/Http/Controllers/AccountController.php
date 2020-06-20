cfebf9225cf689f6aa2ac98df8451bcfba2add6e
minhluan2292 <minhluan2292@gmail.com>
2020-06-20 23:44:44 +0700

    Update Add Account

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AccountController extends Controller
 {
@@ -21,4 +22,29 @@ class AccountController extends Controller
         $accounts = User::all();
         return response()->json($accounts);
     }
+    public function store(Request $request)
+    {
+        if(User::whereEmail($request->email)->count() > 0)
+        {
+            return 'Email exist';
+        }
+
+        // $user = User::create([
+        //     'name' => $request->name,
+        //     'email' => $request->email,
+        //     'password' => Hash::make($request->password),
+        //     'role' => $request->role
+        // ]);
+
+        // return response()->json($user);
+    }
+    public function emailcheck(Request $request)
+    {
+        if(User::whereEmail($request->email)->count() > 0)
+        {
+            return 'Email exist';
+        }
+        return null;
+    }
+
 }
